744b9a347634c0409e9e9968bdc925ad34fb6d1e
Max <forsman.max@gmail.com>
2021-09-05 11:46:46 +0200

    Add basic args and command parsing

--- scripts/context.js
+++ scripts/context.js
@@ -5,7 +5,8 @@ const context = {
     apiUrl: process.env.API_URL,
     privateKey: process.env.PRIVATE_KEY,
     publicKey: process.env.PUBLIC_KEY
-  }
+  },
+  args: process.argv.slice(2)
 }
 
 module.exports = context
--- scripts/contract-interact.js
+++ scripts/contract-interact.js
@@ -7,13 +7,33 @@ const contractAddress = '0xA54661Ab5782e73158426c28e433560cEaB3f553'
 
 
 async function setMessage(){
+  const transaction = {
 
+  }
+}
+
+async function getMessage() {
+  const contract = new web3.eth.Contract(helloContract.abi, contractAddress)
+  const message = await contract.methods.message().call()
+  console.log(message)
 }
 
 async function main() {
-  const contract = new web3.eth.Contract(helloContract.abi, contractAddress)
-  const message = await contract.methods.message().call()
-  console.log(contract)
-  console.log(message)
+  const command = context.args[0]
+  switch(command) {
+    case "read":
+      await getMessage()
+      process.exit(0);
+      break;
+    case "write": {
+      const newMessage = context.args[1]
+      await setMessage(newMessage)
+      process.exit(0)
+    }
+    default: {
+      console.log("Unknown command")
+      process.exit(1)
+    }
+  }
 }
 main()
